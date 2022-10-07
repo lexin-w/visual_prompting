41ed69b9a50ea3aac2e94fbec20af1dba6b0c7a7
Jun Dragonborn <71385903+jasonnoy@users.noreply.github.com>
2022-10-07 13:38:08 +0800

    Update main_clip.py

--- main_clip.py
+++ main_clip.py
@@ -262,7 +262,6 @@ def train(train_loader, texts, model, prompter, optimizer, scheduler, criterion,
 
         optimizer.zero_grad()
         
-        images = torch.stack(images)
         images = images.to(device)
         target = target.to(device)
         text_tokens = clip.tokenize(texts).to(device)
